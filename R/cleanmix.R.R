ac318a70977fdefd855292480bb527c999965974
TinaQian <qyue2014@hotmail.com>
2018-03-10 22:44:10 -0800

    finish first version of Datatype_Cleansing by Tina (code and test)

---
+++ R/cleanmix.R.R
@@ -0,0 +1,55 @@
+#' Clean the mixture of data types
+#'
+#' The function helps to delete the observations with unwanted data types in indicated columns in a data frame.
+#'
+#' @param typemix_result the returned result (a list of 3 data frames) by typemix function.
+#' @param column a number or a vector of numbers indicating the ID of columns where you want to apply the function to remove the mixture of data types
+#' @param type a vector specifying the data types that you want to keep or delete.
+#' @param keep if you want to keep or delete the data type you specify. The default setting is keep=TRUE.
+#'
+#' @return A data frame with observations in unwanted data types in the columns specified deleted.
+#' @export
+#' @examples
+#' cleanmix(typemix(sample_df),c(1,2),type="number")
+#'
+#'
+cleanmix<-function(typemix_result,column,type,keep=TRUE){
+      # turn on warning function
+      options(warn = 1)
+
+      # check input
+      if (class(typemix_result)!="list"){
+            stop('The input should be the result by typemix function')
+      }
+
+      # prepare output data
+      result<-typemix_result[[1]]
+
+      # the code for main function:
+      for (i in 1:length(column)){
+            ID<-column[i]
+            # check if the column has type mixture
+            if (ID %in% typemix_result[[3]]$Column_ID){
+                  # locate the type of data of interest
+                  index<-typemix_result[[2]][ID]==type[i]
+                  # check if the data type exists
+                  if (sum(index)==0){
+                        warning("A column does not have the data type")
+                  } else{
+                        # check if keep or not
+                        if (keep){
+                              result[!index,ID]<-NA
+
+                        } else {
+                              result[index,ID]<-NA
+                        }
+                  }
+
+            } else {
+                  warning("A column does not have type mixture")
+            }
+
+      }
+
+      return(result)
+}
--- tests/testthat/test_Datatype_Cleansing.R
+++ tests/testthat/test_Datatype_Cleansing.R
@@ -1,12 +1,19 @@
+library(testthat)
 # prepare samples
 # for typemix function
 sample<-read.csv("data/test_sample.csv",header = TRUE)
 typemix_output2<-read.csv("data/typemix_output2.csv",header = TRUE)
+typemix_output2$X1<-as.character(typemix_output2$X1)
+typemix_output2$X2<-as.character(typemix_output2$X2)
+typemix_output2$X3<-as.character(typemix_output2$X3)
+typemix_output2$X7<-as.character(typemix_output2$X7)
 typemix_output3<-read.csv("data/typemix_output3.csv",header = TRUE)
 # for cleanmix function
 cleanmix_e1<-read.csv("data/cleanmix_e1.csv",header = TRUE)
-cleanmix_e2<-read.csv("data/cleanmix_e2.csv",header = TRUE)
-cleanmix_e3<-read.csv("data/cleanmix_e3.csv",header = TRUE)
+cleanmix_e1$X2<-as.character(c("test","test",NA,NA))
+cleanmix_e1$X4<-as.numeric(cleanmix_e1$X4)
+cleanmix_e2<-sample
+cleanmix_e2$X1[4]<-NA
 
 test_that("typemix is correct", {
   # check inputs first
@@ -17,26 +24,33 @@ test_that("typemix is correct", {
 
   ## check the code accuracy
   expect_is(typemix(sample),"list")
-  expect_is(typemix(sample)[1],"data.frame")
-  expect_is(typemix(sample)[2],"data.frame")
-  expect_is(typemix(sample)[3],"data.frame")
-  expect_identical(typemix(sample)[1], sample)
-  expect_identical(typemix(sample)[2], typemix_output2)
-  expect_identical(typemix(sample)[3], typemix_output3)
+  expect_is(typemix(sample)[[1]],"data.frame")
+  expect_is(typemix(sample)[[2]],"data.frame")
+  expect_is(typemix(sample)[[3]],"data.frame")
+  expect_identical(typemix(sample)[[1]], sample)
+  expect_identical(typemix(sample)[[2]], typemix_output2)
+  expect_identical(typemix(sample)[[3]], typemix_output3)
 })
+
+result<-typemix(sample)
+
 
 test_that("cleanmix is correct", {
   # check inputs first
-  expect_error(cleanmix(sample,column=c(1,2),type,keep), "The input should be the result by typemix function")
-  expect_warning(cleanmix(typemix(sample),column=c(1,4),type,keep), "Column 4 does not have type mixture")
-  expect_warning(cleanmix(typemix(sample),column=c(1),type="logical",keep), "Column 1 does not have logical data")
+  expect_error(cleanmix(sample,column=c(1,2),type="logical"), "The input should be the result by typemix function")
+  expect_warning(cleanmix(result,column=4,type="logical"), "A column does not have type mixture")
+  expect_warning(cleanmix(result,column=1,type="logical"), "A column does not have the data type")
 
   # check the code accuracy
-  # first example: keep type and keep as default: type=majority, keep=TRUE
-  expect_identical(cleanmix(typemix(sample),column=c(1,2),type,keep), cleanmix_e1)
-  # second example: specify type
-  expect_identical(cleanmix(typemix(sample),column=c(1,2),type="number",keep), cleanmix_e2)
+  # first example: input 2 columns with different data types, keep=TRUE
+  e1<-cleanmix(result,column=c(1,2),type=c("number","character"))
+  e1$X1<-as.numeric(as.character(e1$X1))
+  e1$X2<-as.character(e1$X2)
+  expect_identical(e1, cleanmix_e1)
+  # second example: input 1 column, keep=TRUE
+  e2<-cleanmix(result,column=1,type="number")
+  expect_identical(e2, cleanmix_e2)
   # third example: make keep=FALSE
-  expect_identical(cleanmix(typemix(sample),column=c(1,2),type="character",keep=FALSE), cleanmix_e3)
+  expect_identical(cleanmix(result,column=1,type="character",keep=FALSE), cleanmix_e2)
 
 })
